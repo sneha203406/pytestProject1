ecb879a425d8d53f044bc87da773b5fdbedb54a7
sneha203406 <sneharathi996@gmail.com>
2021-04-14 13:13:37 +0530

    commit

--- tests/main.py
+++ tests/main.py
@@ -1,10 +1,12 @@
 # This is a sample Python script.
 from test_class import TestClass
+
+
 def main():
-    t=TestClass()
+    t = TestClass()
     t.test_one()
     t.test_two()
 
-if __name__ =="__main__":
-    main()
 
+if __name__ == "__main__":
+    main()
--- tests/test_class.py
+++ tests/test_class.py
@@ -1,8 +1,10 @@
 class TestClass:
     def test_one(self):
         x = "sample"
-        assert "l" in x
-
+        if (assert "l" in x):
+            print("value found in x")
+        else:
+            print("value not found in x")
     def test_two(self):
         x = "hello"
         assert hasattr(x, "hello")
