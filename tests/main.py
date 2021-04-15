dd6ba385f68ec5d37dec320e4bfff52b0e2f2dc3
sneha203406 <sneharathi996@gmail.com>
2021-04-15 11:54:01 +0530

    commit

--- tests/main.py
+++ tests/main.py
@@ -1,5 +1,6 @@
 # This is a sample Python script.
 from test_class import TestClass
+from test_sample import TestClass1
 
 
 def main():
@@ -7,6 +8,10 @@ def main():
     t = TestClass()
     t.test_one()
     t.test_two()
+
+    t = TestClass1()
+    t.test_one1()
+    t.test_two2()
 
 
 if __name__ == "__main__":
--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,8 +1,8 @@
-class TestClass:
-    def test_one(self):
+class TestClass1:
+    def test_one1(self):
         x = "sample"
         assert "l" in x
 
-    def test_two(self):
+    def test_two2(self):
         x = "hello"
-        assert hasattr(x, "hello")
+        assert "e" in x
